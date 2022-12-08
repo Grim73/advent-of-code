829c236837e02d6d9eabf62443b140e0fdffa5b6
Grim73 <102001567+Grim73@users.noreply.github.com>
2022-12-08 15:38:14 -0600

    adding input script

---
+++ inputPull.py
@@ -0,0 +1,19 @@
+import os
+
+def getToday():
+    year = input("What year: ")
+    day = input("What day: ")
+    token = input("What is the name of the file holding your session token: ")
+    f = open(token,"r")
+    cookie  = f.readline()
+    
+    dest = input("What is the destiniation file name: ")
+    file = open(dest, "w")
+
+    url = f"https://adventofcode.com/{year}/day/{day}/input"
+    print(url)
+    input()
+    os.system(f'curl -o input.txt "{url}" --cookie {cookie}')
+
+getToday()
+ 
