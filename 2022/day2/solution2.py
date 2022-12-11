427156ab62b98d7691ad2d14aef955be68821338
Grim73 <grimreepr73@gmail.com>
2022-12-10 19:24:53 -0600

    updates

--- 2022/day2/solution2.py
+++ 2022/day2/solution2.py
@@ -4,17 +4,28 @@ lines = f.readlines()
 games = [entry.strip() for entry in lines]
 
 win = ["C X", "A Y", "B Z"]
+loss = ["B X", "C Y",  "A Z"]
 rock = []
 paper = []
 scissors = []
 draw = []
 for entry in games:
+    
     if entry == win[0]:
         rock.append(entry)
+
     elif entry == win[1]:
         paper.append(entry)
+    
+    elif entry == win[2]:
+        scissors.append(entry)
+
+    elif entry == loss[0]:
+
     else:
+        
 
 
+##["B X", "C Y",  "A Z"]
 print(len(rock))
 
