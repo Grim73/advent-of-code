db94d2eefa24af2e021cd092881aa672548fc654
Grim73 <grimreepr73@gmail.com>
2022-12-22 20:07:45 -0600

    updates

--- 2022/day3/solution.py
+++ 2022/day3/solution.py
@@ -1,7 +1,71 @@
 f = open("input.txt", "r")
 lines = f.readlines()
 ## one line function adding each occurence of `lines` to the list `calories`
-games = [entry.strip() for entry in lines]
+rucksack = [entry.strip() for entry in lines]
 
-for entry in games:
-    entry
+priority = [
+        "a":1,
+        "b":2,
+        "c":3,
+        "d":4,
+        "e":5,
+        "f":6,
+        "g":7,
+        "h":8,
+        "i":9,
+        "j":10,
+        "k":11,
+        "l":12,
+        "m":13,
+        "n":14,
+        "o":15,
+        "p":16,
+        "q":17,
+        "r":18,
+        "s":19,
+        "t":20,
+        "u":21,
+        "v":22,
+        "w":23,
+        "x":24,
+        "y":25,
+        "z":26,
+        "A":27,
+        "B":28,
+        "C":29,
+        "D":30,
+        "E":31,
+        "F":32,
+        "G":33,
+        "H":34,
+        "I":35,
+        "J":36,
+        "K":37,
+        "L":38,
+        "M":39,
+        "N":40,
+        "O":41,
+        "P":42,
+        "Q":43,
+        "R":44,
+        "S":45,
+        "Y":46,
+        "U":47,
+        "V":48,
+        "W":49,
+        "X":50,
+        "Y":51,
+        "Z":52,
+        ],
+,
+count= 0
+
+for entry in rucksack:
+
+    ##TODO split each `entry` in half
+    ##DONE make a `priority` list(letters == num 1-52)
+    ##TODO compare each split `entry` to the other half
+    ##TODO index matching numbers, add them to the count
+    ##     based on `priority`
+
+
